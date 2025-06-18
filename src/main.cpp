16890e4885fe9797df9db00ca979b7dfe3fe0e76
shadow-foss <144371189+shadow-foss@users.noreply.github.com>
2025-06-19 00:30:42 +0530

    fix(main): reorder includes for logical consistency

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
+#include "../include/path_utils.hpp"
 #include "../include/screen_manager.hpp"
 #include "../include/settings.hpp"
 #include "../include/ui_manager.hpp"
 #include "../include/window_manager.hpp"
-#include "../include/path_utils.hpp"
 #include "raylib.h"
 int main() {
   createWindow();
