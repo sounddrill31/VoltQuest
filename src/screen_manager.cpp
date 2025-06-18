dd97088243d699d8fb73f19b3bff610381321bc2
shadow-foss <144371189+shadow-foss@users.noreply.github.com>
2025-06-19 00:29:32 +0530

    refactor(screen): implement button focus logic and UI scaling updates
    - Added per-screen button focus tracking
    - Replaced DrawText with drawUIText
    - Adjusted font sizes for consistency

--- src/screen_manager.cpp
+++ src/screen_manager.cpp
@@ -3,14 +3,15 @@
 #include "../include/ui_manager.hpp"
 #include "raylib.h"
 
-constexpr float screenW = 1920.0f;
-constexpr float screenH = 1080.0f;
+const float screenW = 1920.0f;
+const float screenH = 1080.0f;
 
 enum class SCREEN { START_MENU, OPTIONS_MENU, CHAPTER_MENU, LEVEL_MENU, GAME };
 SCREEN currentScreen = SCREEN::START_MENU;
 
 namespace startMenu {
-constexpr float logoSize = 450.0f;
+int focusedButton = 0;
+const float logoSize = 450.0f;
 static Vector2 buttonSize = {360.0f, 120.0f};
 static Rectangle logoBounds = {screenW / 2.0f - startMenu::logoSize / 2.0f,
                                screenH / 2.0f - startMenu::logoSize,
@@ -20,21 +21,21 @@ static imageButton playButton = {
     {screenW / 2.0f - startMenu::buttonSize.x / 2.0f, screenH / 2.0f,
      startMenu::buttonSize.x, startMenu::buttonSize.y},
     "PLAY",
-    50,
+    55,
     Color{255, 198, 0, 255}};
 
 static imageButton optionsButton = {
     {screenW / 2.0f - startMenu::buttonSize.x / 2.0f, screenH / 2.0f + 160.f,
      startMenu::buttonSize.x, startMenu::buttonSize.y},
     "OPTIONS",
-    50,
+    55,
     Color{0, 146, 255, 255}};
 
 static imageButton quitButton = {
     {screenW / 2.0f - startMenu::buttonSize.x / 2.0f, screenH / 2.0f + 320.f,
      startMenu::buttonSize.x, startMenu::buttonSize.y},
     "QUIT",
-    50,
+    55,
     RED};
 
 imageButton *buttonsArray[] = {&playButton, &optionsButton, &quitButton};
@@ -45,6 +46,7 @@ static Vector2 panelSize = {1000.0f, 900.0f};
 static Rectangle panelBounds = {screenW / 2.0f - panelSize.x / 2.0f,
                                 screenH / 2.0f - panelSize.y / 2.0f,
                                 panelSize.x, panelSize.y};
+static Vector2 optionsTextPos = {screenW / 2.0f, 140.0f};
 } // namespace optionsMenu
 
 void drawStartMenu() {
@@ -58,15 +60,16 @@ void drawStartMenu() {
 void drawOptionsMenu() {
   ClearBackground(Color{58, 71, 80, 255});
   drawImage(IMGPANEL, optionsMenu::panelBounds);
-  DrawText("OPTIONS", screenW / 2.0f - MeasureText("OPTIONS", 80) / 2.0f,
-           140.0f, 80, Color{0, 146, 255, 255});
+  drawUIText(80, optionsMenu::optionsTextPos, "OPTIONS",
+             Color{0, 146, 255, 255});
 }
 
 void drawCurrentScreen() {
   switch (currentScreen) {
   case SCREEN::START_MENU: {
     drawStartMenu();
-    updateKeyboardNavigation(startMenu::buttonsArray, 3);
+    updateKeyboardNavigation(3, startMenu::focusedButton,
+                             startMenu::buttonsArray);
     if (isImageButtonPressed(startMenu::playButton)) {
       currentScreen = SCREEN::CHAPTER_MENU;
     }
